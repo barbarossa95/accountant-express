ff7be05cf2563ac766075bfae406584114e9cde4
greshilov.v <greshilov@a2design.biz>
2019-04-14 19:15:34 +0600

    updated: operation api endpoint

--- routes/api/operation.js
+++ routes/api/operation.js
@@ -3,10 +3,18 @@ var router = express.Router();
 
 var operationRouter = require("./operation");
 
-router.get("/", function(req, res) {
-  res.json({
-    test: "balalayka"
+router
+  .get("/", function(req, res) {
+    res.json(initOperations);
+  })
+  .post("/", function(req, res) {
+    const operation = req.body;
+
+    console.log("New operation posted");
+
+    console.log(operation);
+
+    res.json(operation);
   });
-});
 
 module.exports = router;
