1291df21c987828ad7d4a74bbbf917e3e9be179b
Vladimir Greshilov <greshilov@a2design.biz>
2019-04-18 19:45:39 +0600

    added: builded front in public\

---
+++ public/precache-manifest.f68861c87af5efa0d210e9eb52344d5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "343cc67f2dbb30fe0e6e",
+    "url": "/static/js/main.343cc67f.chunk.js"
+  },
+  {
+    "revision": "921d2681f51d8587a507",
+    "url": "/static/js/2.921d2681.chunk.js"
+  },
+  {
+    "revision": "343cc67f2dbb30fe0e6e",
+    "url": "/static/css/main.ad12650f.chunk.css"
+  },
+  {
+    "revision": "df25b16ca7ca0b131e7b7f65fa2f6f48",
+    "url": "/index.html"
+  }
+];
